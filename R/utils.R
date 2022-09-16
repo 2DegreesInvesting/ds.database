e995c33cdbe8c8f2652ed97576bf3d7f042ce58e
Mauro Lepore <maurolepore@gmail.com>
2022-09-16 12:05:52 +0000

    Refactor

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,11 @@
-cols_companies = function(path) {
+cols_companies = function() {
   vroom::cols(
     companies_id = vroom::col_character(),
     information = vroom::col_character()
   )
 }
 
-cols_categories = function(path) {
+cols_categories = function() {
   vroom::cols(
     companies_id = vroom::col_character(),
     sector = vroom::col_character()
