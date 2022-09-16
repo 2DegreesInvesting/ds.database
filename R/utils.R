274692b7f1f7ee0565fce5ed3f99aac4ac0a0e25
Mauro Lepore <maurolepore@gmail.com>
2022-09-16 12:06:21 +0000

    Style

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,11 @@
-cols_companies = function() {
+cols_companies <- function() {
   vroom::cols(
     companies_id = vroom::col_character(),
     information = vroom::col_character()
   )
 }
 
-cols_categories = function() {
+cols_categories <- function() {
   vroom::cols(
     companies_id = vroom::col_character(),
     sector = vroom::col_character()
