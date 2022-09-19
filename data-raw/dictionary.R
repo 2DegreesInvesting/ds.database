8182a6b865a4a7e87af09362a84be009ff2ccd69
Mauro Lepore <maurolepore@gmail.com>
2022-09-19 14:21:13 +0000

    Fix name

--- data-raw/dictionary.R
+++ data-raw/dictionary.R
@@ -1,5 +1,5 @@
 library(here)
 library(vroom)
 
-dictionary <- vroom(here("data-raw", "dicionary.tsv"), col_types = "ccccll")
+dictionary <- vroom(here("data-raw", "dictionary.tsv"), col_types = "ccccll")
 use_data(dictionary, overwrite = TRUE)
